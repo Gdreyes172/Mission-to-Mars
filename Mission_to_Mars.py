dafdfe3edb77470abcd4a337d0c11aecacc82136
Gdreyes172 <reyesgr11@gmail.com>
2022-03-13 16:59:37 -0500

    update

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -7,58 +7,71 @@ import pandas as pd
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com/'
-browser.visit(url)
+# Changing our code into a function.
+def mars_news(browser):
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com/'
+    browser.visit(url)
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-# Convert the browser html to soup object and then quit the browser
-## Setting up the HTML parser: 
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
+    # Convert the browser html to soup object and then quit the browser
+    ## Setting up the HTML parser: 
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    
+    except AttributeError:
+        return None, None
 
-slide_elem.find('div', class_='content_title')
+    return news_title, news_p
 
-## Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+def featured_image(browser):
 
-## Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    # ## JPL Space images Featured Images
+    ## Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# ## JPL Space images Featured Images
-## Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    ## Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-## Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    ## Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+    
+    try:
+       # find the relative image url
+       img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    
+    except AttributeError:
+        return None
 
-## Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    ## Use the base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'    
+    return img_url
+    
+def mars_facts():  
+    # ## Mars Facts
+    ## Using.read_html() to create a DataFrame using pandas
+    # Add try/except for error handling
+    try:
+        # use 'read_html" to scrape the facts table into a dataframe
+        marspp_df = pd.read_html('https://galaxyfacts-mars.com')[0]
 
-## Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
-
-## Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
-
-# ## Mars Facts
-## Visit URL
-url = 'https://galaxyfacts-mars.com'
-browser.visit(url)
-## Using.read_html() to create a DataFrame using pandas
-marspp_df = pd.read_html('https://galaxyfacts-mars.com')[0]
-marspp_df.columns=['description', 'Mars', 'Earth']
-marspp_df.set_index('description', inplace=True)
-marspp_df
-marspp_df.to_html()
-browser.quit()
+    except BaseException:
+        return None
+    # Assign column names and set the index of the dataframe.
+    marspp_df.columns=['description', 'Mars', 'Earth']
+    marspp_df.set_index('description', inplace=True)
+       
+    # Convert dataframe into HTML format, add bootstrap
+    return marspp_df.to_html()
--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-# Import dependencies
-from splinter import Browser
-from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
@@ -17,17 +12,19 @@ mongo = PyMongo(app)
 # Defining the HTML routes
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
 # Next route that will scrape our data
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update_one({}, {"$set":mars_data}, upsert=True)
-    return redirect('/', code=302)
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
 
 # Tell Flask to run
 if __name__ == "__main__":
+   app.debug = True
    app.run()
+   
